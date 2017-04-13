d380aad04835c6bdaefd211928d6b7c8a6ad96b0
Robin Meijer <Robin Meijer>
2017-04-13 20:36:22 +0200

    Overlay update

---
+++ js/overlay.js
@@ -0,0 +1,25 @@
+var canvas;
+var fr = 60;
+
+function setup() {
+    pixelDensity(1);
+    frameRate(fr);
+    canvas = createCanvas(windowWidth, windowHeight);
+    canvas.parent('canvasContainer');
+}
+
+function showReticle() {
+    var x = floor(mouseX / 20) * 20;
+    var y = floor(mouseY / 20) * 20;
+    noFill();
+    stroke(255, 100, 0);
+    strokeWeight(1);
+    rect(x, y, 20, 20);
+}
+
+function draw() {
+    background(200);
+    showReticle();
+    // camera(0, 0, 10);
+    // plane(100,100);
+}
