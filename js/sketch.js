3f183ae7f2b9ac8e746b625a503548c13ab9e899
bramdoppen <bramdoppen@gmail.com>
2017-04-17 23:06:50 +0200

    Toevoegen van google login (nog niet afgerond, maar website werkt wel gewoon)

--- js/sketch.js
+++ js/sketch.js
@@ -122,3 +122,29 @@ function errData(err){
 function draw() {
 //    showReticle();
 }
+
+var provider = new firebase.auth.GoogleAuthProvider();
+
+function login() {
+    firebase.auth().signInWithRedirect(provider);
+
+    firebase.auth().getRedirectResult().then(function(result) {
+        if (result.credential) {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            var token = result.credential.accessToken;
+            // ...
+        }
+        // The signed-in user info.
+        var user = result.user;
+    }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+    });
+    console.log(user);
+
+}
